643f7765e0f613b0bde05e3be5f49d3c2d37a61e
Marvin Rausch <marvin.rausch@live.de>
2021-06-02 11:56:53 +0200

    added missing file for integration tests

---
+++ tests/net/sharksystem/lora_integration_test/ConnectorInternalLocalStub.java
@@ -0,0 +1,46 @@
+package net.sharksystem.lora_integration_test;
+
+import net.sharksystem.hub.ASAPHubException;
+import net.sharksystem.hub.StreamPair;
+import net.sharksystem.hub.StreamPairImpl;
+import net.sharksystem.hub.hubside.ConnectorInternal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ConnectorInternalLocalStub implements ConnectorInternal {
+
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public ConnectorInternalLocalStub(InputStream inputStream, OutputStream outputStream) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+    }
+
+    @Override
+    public void connectionRequest(CharSequence sourcePeerID, CharSequence targetPeerID, int timeout) throws ASAPHubException, IOException {
+
+    }
+
+    @Override
+    public void disconnect(CharSequence sourcePeerID, CharSequence targetPeerID) throws ASAPHubException {
+
+    }
+
+    @Override
+    public void startDataSession(CharSequence sourcePeerID, CharSequence targetPeerID, StreamPair connection, int timeout) throws ASAPHubException, IOException {
+
+    }
+
+    @Override
+    public void notifyConnectionEnded(CharSequence sourcePeerID, CharSequence targetPeerID, StreamPair connection) throws ASAPHubException {
+
+    }
+
+    @Override
+    public StreamPair initDataSession(CharSequence sourcePeerID, CharSequence targetPeerID, int timeout) throws ASAPHubException, IOException {
+        return new StreamPairImpl(this.inputStream, this.outputStream, "test");
+    }
+}
