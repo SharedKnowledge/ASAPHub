d74f147c065a70eceb1c3346a164cbf6c88421b9
Marvin Rausch <s0566563@htw-berlin.de>
2023-01-23 12:53:41 +0100

    HubConnectorCLI.java: add history command (print to console + save as file)

--- src/main/java/net/sharksystem/hub/HubConnectorCLI.java
+++ src/main/java/net/sharksystem/hub/HubConnectorCLI.java
@@ -9,7 +9,9 @@ import net.sharksystem.utils.Commandline;
 
 import java.beans.ExceptionListener;
 import java.io.*;
-import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class HubConnectorCLI {
@@ -18,29 +20,48 @@ public class HubConnectorCLI {
     static final String CONNECT_PEER = "connect";
     static final String HELP = "help";
     static final String EXIT = "exit";
+    static final String HISTORY = "history";
     private final PrintStream printStream;
     private final InputStream inputStream;
     private int port;
     static int DEFAULT_PORT = 6000;
     private String hostName;
     private CLIConnectionListener connectionListener;
+    private List<String> commandHistory;
 
     private static String helpText =
             "list available peers:         peers\n" +
                     "set own peer-id               set-id [peer-id] [multichannel false/true]\n" +
                     "connect to peer               connect [target-peer-id]\n" +
                     "get cli guidelines            help\n" +
+                    "print history                 history\n" +
+                    "export history                history [file-name/path]\n" +
                     "terminate application         exit";
 
-
+    /**
+     * constructor for ConnectorCLI
+     * @param inputStream stream for reading commands
+     * @param outputStream stream which should be used for the CLI output
+     * @param host host/IP of the ASAPHub
+     * @param port port of the ASAPHub
+     */
     public HubConnectorCLI(InputStream inputStream, OutputStream outputStream, String host, int port) {
         printStream = new PrintStream(outputStream);
         this.inputStream = inputStream;
         this.port = port;
         this.hostName = host;
         connectionListener = new CLIConnectionListener(this.printStream);
+        commandHistory = new ArrayList<>();
     }
 
+    /**
+     * alternative constructor for ConnectorCLI
+     * @param inputStream stream for reading commands
+     * @param outputStream stream which should be used for the CLI output
+     * @param host host/IP of the ASAPHub
+     * @param port port of the ASAPHub
+     * @param exceptionListener listener which is called if IO Exception is thrown inside HubConnector
+     */
     public HubConnectorCLI(InputStream inputStream, OutputStream outputStream, String host, int port, ExceptionListener exceptionListener) {
         this(inputStream, outputStream, host, port);
         connectionListener = new CLIConnectionListener(this.printStream, exceptionListener);
@@ -55,11 +76,11 @@ public class HubConnectorCLI {
             String line;
             printStream.println("connector CLI started");
             while ((line = in.readLine()) != null) {
-                String command = line;
+                boolean commandIsValid = true;
                 List<String> args = new ArrayList<>();
                 // get command and argument
-                String[] commandAndArg = command.split(" ");
-                command = commandAndArg[0];
+                String[] commandAndArg = line.split(" ");
+                String command = commandAndArg[0];
                 if (commandAndArg.length > 1)
                     args = new ArrayList<>(Arrays.asList(Arrays.copyOfRange(commandAndArg, 1, commandAndArg.length)));
                 printStream.println(command);
@@ -87,15 +108,42 @@ public class HubConnectorCLI {
                     case EXIT:
                         System.exit(0);
                         break;
+                    case HISTORY:
+                        if(args.isEmpty()){
+                            // print history to console
+                            commandHistory.forEach(printStream::println);
+                        }else {
+                            // save history as file
+                            saveHistoryAsFile(args.get(0));
+                        }
+                        break;
                     default:
+                        commandIsValid = false;
                         printStream.println(helpText);
                         break;
                 }
-                Thread.sleep(2000);
+                if(commandIsValid){
+                    commandHistory.add(line);
+                    Thread.sleep(2000);
+                }
             }
         } catch (ASAPException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * saves the command history as file
+     * @param fileName file name or path
+     * @throws IOException
+     */
+    public void saveHistoryAsFile(String fileName) throws IOException {
+        Path filePath = Paths.get(fileName);
+        printStream.println(filePath.toAbsolutePath());
+        PrintWriter pw = new PrintWriter(Files.newOutputStream(filePath));
+        for (String command : commandHistory)
+            pw.println(command);
+        pw.close();
     }
 
     public static void main(String[] args) throws ASAPException, IOException, InterruptedException {
@@ -104,7 +152,6 @@ public class HubConnectorCLI {
                 false, usageString);
 
         int port = DEFAULT_PORT;
-        int maxIdleInSeconds = -1;
 
         if (argumentMap != null) {
             Set<String> keys = argumentMap.keySet();
@@ -129,11 +176,6 @@ public class HubConnectorCLI {
                 System.err.println("hostname not set");
                 System.exit(1);
             }
-            String multiChannelStr = argumentMap.get("-multichannel");
-            boolean multiChannel = false;
-            if (multiChannelStr != null) {
-                multiChannel = Boolean.valueOf(multiChannelStr);
-            }
 
             PrintStream o = new PrintStream("log.txt");
             PrintStream console = System.out;
@@ -144,7 +186,6 @@ public class HubConnectorCLI {
             HubConnectorCLI cli = new HubConnectorCLI(System.in, console, host, port);
 
             cli.startCLI();
-
         }
     }
 }
